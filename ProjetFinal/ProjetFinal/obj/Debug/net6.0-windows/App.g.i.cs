253db47eed53d4bccb77830509fea92405b58fe6
AmelieRacine08 <141154177+AmelieRacine08@users.noreply.github.com>
2023-11-01 12:06:39 -0400

    Amelie

--- ProjetFinal/ProjetFinal/obj/Debug/net6.0-windows/App.g.i.cs
+++ ProjetFinal/ProjetFinal/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ProjetFinal {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ProjetFinal {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public static void Main() {
             ProjetFinal.App app = new ProjetFinal.App();
             app.InitializeComponent();
--- ProjetFinal/ProjetFinal/obj/Debug/net6.0-windows/User Controls/TabConsulterData.g.i.cs
+++ ProjetFinal/ProjetFinal/obj/Debug/net6.0-windows/User Controls/TabConsulterData.g.i.cs
@@ -47,13 +47,13 @@ namespace ProjetFinal.User_Controls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ProjetFinal;component/user%20controls/tabconsulterdata.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProjetFinal;V1.0.0.0;component/user%20controls/tabconsulterdata.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\User Controls\TabConsulterData.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace ProjetFinal.User_Controls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
