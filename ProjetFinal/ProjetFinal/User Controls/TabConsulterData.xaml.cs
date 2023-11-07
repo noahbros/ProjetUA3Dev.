c5ce341390ffe106a6808acea53929d9cb0b4087
Olivier Caron <olcaron@hotmail.com>
2023-11-07 16:02:56 -0500

    Première version de la page consulter
    - créer base de la page consulter
    - créer listeview et grid view
    - Populer gridview
    - ajouter code de base pour grid view

--- ProjetFinal/ProjetFinal/User Controls/TabConsulterData.xaml.cs
+++ ProjetFinal/ProjetFinal/User Controls/TabConsulterData.xaml.cs
@@ -18,11 +18,27 @@ namespace ProjetFinal.User_Controls
     /// <summary>
     /// Interaction logic for TabConsulterData.xaml
     /// </summary>
+    /// 
+
+    public class Stagiaire
+    {
+        public string Prenom { get; set; }
+        public string NomDeFamille { get; set; }
+        public int NumeroEtudiant { get; set; }
+        public string DateDeNaissance { get; set; }
+        public string Sexe {  get; set; }
+        public string Programme { get; set; }
+    }
+
     public partial class TabConsulterData : UserControl
     {
         public TabConsulterData()
         {
             InitializeComponent();
+
+            List<Stagiaire> items = new List<Stagiaire>();
+            items.Add(new Stagiaire() { Prenom = "Olivier", NomDeFamille = "Caron", NumeroEtudiant = 2680133, DateDeNaissance = "2002/11/10", Sexe = "Male", Programme = "Programmation Informatique" });
+            lvConsulter.ItemsSource = items;
         }
     }
 }
